83b87fe2548f1db9f2217a99111d3d128fc6aaa5
glitchlesscode <timothy.veugelers@gmail.com>
2023-09-03 16:00:56 -0600

    Testing Websockets

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const WebSocket = require("ws");
+const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("Hello! Message From Server!!");
+});
