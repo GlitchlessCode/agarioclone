44a4f466ccd6246bab183041d0cc13fd15765f8c
glitchlesscode <timothy.veugelers@gmail.com>
2023-09-04 10:00:48 -0600

    Adding Express for 200 OK

--- app/index.js
+++ app/index.js
@@ -1,9 +1,20 @@
 const WebSocket = require("ws");
-const PORT = process.env.PORT || 3000;
-const wss = new WebSocket.Server({ port: PORT });
+const express = require("express");
+
+const app = express();
+
+const wss = new WebSocket.Server({ port: 3000 });
 wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     console.log(`Received message => ${message}`);
   });
   ws.send("Hello! Message From Server!!");
+});
+
+app.get("/", async (req, res) => {
+  res.status(200).send();
+});
+
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
 });
