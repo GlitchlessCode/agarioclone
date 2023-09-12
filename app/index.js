6667f5c74251d119db24cc59d6681f1dd5d567b5
glitchlesscode <timothy.veugelers@gmail.com>
2023-09-11 23:14:23 -0600

    Switch over packages to fix docker websocket issue

--- app/index.js
+++ app/index.js
@@ -1,18 +1,25 @@
+// Import Statements
 const express = require("express");
+const ws = require("ws");
 const app = express();
-const expressWs = require("express-ws")(app);
 
-app.get("/", function (req, res, next) {
+// Websocket Server
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on("connection", (socket) => {
+  console.log("socket");
+  socket.on("message", (message) => console.log(message.toString()));
+});
+
+// Express Server
+const server = app.listen(3000);
+console.log("listening");
+server.on("upgrade", (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head, (socket) => {
+    wsServer.emit("connection", socket, request);
+  });
+});
+
+app.get("/", (req, res) => {
   console.log("get route");
   res.end();
 });
-
-app.ws("/", function (ws, req) {
-  ws.on("message", function (msg) {
-    console.log(msg);
-  });
-  console.log("socket");
-});
-
-app.listen(3000);
-console.log("listening");
